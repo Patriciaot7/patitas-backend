037be8e716b174de041185e43de7c62b6956b318
Patricia <patriciaevelyn2005@gmail.com>
2024-09-21 12:12:44 -0500

    Carga inicial del proyecto

---
+++ src/test/java/pe/edu/cibetec/patitas_backend/PatitasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibetec.patitas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
