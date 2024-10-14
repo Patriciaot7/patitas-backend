1696476e76ce73e62a7ab03f45826d47090d00cb
Patricia <patriciaevelyn2005@gmail.com>
2024-10-14 10:51:21 -0500

    validaciones backend

---
+++ src/main/java/pe/edu/cibetec/patitas_backend/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.cibetec.patitas_backend.dto;
+
+public record LoginResponseDTO(String codigo, String mensaje, String nombreUsuario, String correoUsuario) {
+}
---
+++ src/main/java/pe/edu/cibetec/patitas_backend/service/AutenticacionService.java
@@ -0,0 +1,11 @@
+package pe.edu.cibetec.patitas_backend.service;
+
+import pe.edu.cibetec.patitas_backend.dto.LoginRequestDTO;
+
+import java.io.IOException;
+
+public interface AutenticacionService {
+
+    String[] validarUsuario(LoginRequestDTO loginRequestDTO) throws IOException;
+
+}
